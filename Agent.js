436b9c63f9b5f4f4397b163d597281e8054e1b1c
snicov <152130745+snicov@users.noreply.github.com>
2024-07-09 15:43:15 +0300

    Add files via upload

--- Agent.js
+++ Agent.js
@@ -1,38 +1,52 @@
 class Agent {
-        constructor(x, y, length, size, col, direction, numOffsprings, layers) {
-          this.x = x;
-          this.y = y;
-          this.length = length;
-          this.size = size;
-          this.col = col;
-          this.direction = direction;
-          this.numOffsprings = numOffsprings;
-          this.layers = layers;
-        }
+    constructor(a) {
+        this.x = a[0];
+        this.y = a[1];
+        this.length = a[2];
+        this.size = a[3];
+        this.col = color(a[4], a[5], a[6]);
+        this.direction = a[7];
+        this.numOffsprings = a[8];
+        this.layers = a[9];
+    }
 
-        show() {
-          strokeWeight(this.size);
-          stroke(this.col);
-          let endX = this.x + this.length * sin(this.direction);
-          let endY = this.y + this.length * cos(this.direction);
-          line(this.x, this.y, endX, endY);
+    show() {
+        strokeWeight(this.size);
+        stroke(this.col);
+        let endX = this.x + this.length * sin(radians(this.direction));
+        let endY = this.y + this.length * cos(radians(this.direction));
+        line(this.x, this.y, endX, endY);
 
-          // Produce offspring agents
-          if (this.layers > 0) {
+        // Produce offspring agents
+        if (this.layers > 0) {
             for (let i = 0; i < this.numOffsprings; i++) {
+                let newDirection = this.direction + random(-30, 30);
+                let newLength = max(this.length + random(-5, 5), 0);
+                let newColor = color(
+                    red(this.col) + random(-20, 20),
+                    green(this.col) + random(-20, 20),
+                    blue(this.col) + random(-20, 20)
+                );
+                let newNumOffsprings = this.numOffsprings + 2;
+                let newSize = this.size * 0.8;
+                let newLayers = this.layers - 1;
 
-              
-              let newDirection = this.direction + radians(random(-30, 30)); // Slightly randomize direction for each offspring
-              
-              let newLength = max(this.length + random(-5,5),0)
-              
-              let newColor = color(red(this.col) + random(-10,10), green(this.col) + random(-10, 10), blue(this.col) + random(-10, 10)) 
-              
-              let offspring = new Agent(endX, endY, newLength * 0.8, this.size, newColor, newDirection, this.numOffsprings, this.layers - 1);
+                let newA = [
+                    endX,
+                    endY,
+                    newLength,
+                    newSize,
+                    red(newColor),
+                    green(newColor),
+                    blue(newColor),
+                    newDirection,
+                    newNumOffsprings,
+                    newLayers
+                ];
 
-
-              offspring.show();
+                let offspring = new Agent(newA);
+                offspring.show();
             }
-          }
         }
-      }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -2,8 +2,12 @@ function setup() {
         createCanvas(800, 800);
         background(255);
 
+
+        let array1 = [400, 400, 50, 5, 100, 102, 153, 180, 2, 5]
+
         let agentColor = color(100, 102, 153);
-        let initialAgent = new Agent(400, 400, 50, 5, agentColor, radians(180), 2, 5);
+        let initialAgent = new Agent(array1);
+        //let initialAgent = new Agent(400, 400, 50, 5, agentColor, radians(180), 2, 5);
         initialAgent.show();
       }
 
