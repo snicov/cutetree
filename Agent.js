916d975f0210281ccd5de74fd99041962dd31fa5
snicov <152130745+snicov@users.noreply.github.com>
2024-07-21 14:02:49 +0300

    Add files via upload

--- Agent.js
+++ Agent.js
@@ -1,50 +1,43 @@
-class Agent {
-    constructor(a) {
-        this.x = a[0];
-        this.y = a[1];
-        this.length = a[2];
-        this.size = a[3];
-        this.col = color(a[4], a[5], a[6]);
-        this.direction = a[7];
-        this.numOffsprings = a[8];
-        this.layers = a[9];
+class Tree {
+    constructor(a, layer, x, y, angle) {
+        this.a = a;
+        this.layer = layer;
+        this.x = x;
+        this.y = y;
+
+        // Extract parameters for the current layer
+        this.length = a[layer];
+        this.length_deviation = a[10 + layer];
+        this.size = a[20 + layer];
+        this.red = a[30 + layer];
+        this.green = a[40 + layer];
+        this.blue = a[50 + layer];
+        this.red_change = a[60 + layer];
+        this.green_change = a[70 + layer];
+        this.blue_change = a[80 + layer];
+        this.branches = a[90 + layer];
+        this.angle_deviation = a[100 + layer]
+        this.angle = angle;
     }
 
     show() {
         strokeWeight(this.size);
-        stroke(this.col);
-        let endX = this.x + this.length * sin(radians(this.direction));
-        let endY = this.y + this.length * cos(radians(this.direction));
+        let col = color(
+            this.red + random(this.red_change),
+            this.green + random(this.green_change),
+            this.blue + random(this.blue_change)
+        );
+        stroke(col);
+
+        let deviated_angle = this.angle + random(-this.angle_deviation, this.angle_deviation);
+        let endX = this.x + (this.length + random(-this.length_deviation, this.length_deviation)) * sin(radians(deviated_angle));
+        let endY = this.y - (this.length + random(-this.length_deviation, this.length_deviation)) * cos(radians(deviated_angle));
         line(this.x, this.y, endX, endY);
 
-        // Produce offspring agents
-        if (this.layers > 0) {
-            for (let i = 0; i < this.numOffsprings; i++) {
-                let newDirection = this.direction + random(-30, 30);
-                let newLength = max(this.length + random(-5, 5), 0);
-                let newColor = color(
-                    red(this.col) + random(-20, 20),
-                    green(this.col) + random(-20, 20),
-                    blue(this.col) + random(-20, 20)
-                );
-                let newNumOffsprings = this.numOffsprings + 2;
-                let newSize = this.size * 0.8;
-                let newLayers = this.layers - 1;
-
-                let newA = [
-                    endX,
-                    endY,
-                    newLength,
-                    newSize,
-                    red(newColor),
-                    green(newColor),
-                    blue(newColor),
-                    newDirection,
-                    newNumOffsprings,
-                    newLayers
-                ];
-
-                let offspring = new Agent(newA);
+        // Produce offspring branches
+        if (this.layer < 9) { // Only produce offspring if not on the last layer
+            for (let i = 0; i < this.branches; i++) {
+                let offspring = new Tree(this.a, this.layer + 1, endX, endY, deviated_angle);
                 offspring.show();
             }
         }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,48 @@
+let trees = [];
+let saveIndex = 0;
+
+
+
 function setup() {
-        createCanvas(800, 800);
-        background(255);
+    createCanvas(800, 800);
 
+    for (let i = 0; i < 1000; i++) {
+        let array = createArray();
+        trees.push(new Tree(array, 0, 400, 600, 0));
+    }
 
-        let array1 = [400, 400, 50, 5, 100, 102, 153, 180, 2, 5]
+    saveNextTree();
 
-        let agentColor = color(100, 102, 153);
-        let initialAgent = new Agent(array1);
-        //let initialAgent = new Agent(400, 400, 50, 5, agentColor, radians(180), 2, 5);
-        initialAgent.show();
-      }
+}
 
-      function draw() {
-        // No continuous drawing needed
-        noLoop();
-      }
+function createArray() {
+    let the_array = [
+        50 + random(50), 40 + random(40), 30 + random(30), 25 + random(25), 20 + random(20), 15 + random(15), 12 + random(12), 10 + random(10), 7 + random(7), 5 + random(5),
+        random(5), random(5), random(5), random(3), random(3), random(3), random(2), random(2), random(2), random(1), // length deviation
+        5 + random(2), 4 + random(2), 3 + random(2), 3 + random(2), 3 + random(2), 2 + random(1), 2 + random(1), 2 + random(1), 1 + random(1), random(1), // size
+        255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), // red
+        255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), // green
+        255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), 255 * random(1), // blue
+        10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), // red change
+        10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), // green change
+        10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), 10 * random(1), // blue change
+        2 + random(2), 2 + random(2), 2 + random(2), 2 , 4, 4, 4, 4, 4, 4, // number of branches
+        0, random(120), random(120), random(120), random(120), random(120), random(120), random(120), random(120), random(120) // angle deviation
+    ];
+
+    return the_array;
+}
+
+function saveNextTree() {
+    if (saveIndex < trees.length) {
+        background(255); // Clear the canvas for each new tree
+        trees[saveIndex].show();
+        save(`img${saveIndex}.png`);
+        saveIndex++;
+        setTimeout(saveNextTree, 500); // Delay before saving the next tree
+    }
+}
+
+function draw() {
+    noLoop();
+}
